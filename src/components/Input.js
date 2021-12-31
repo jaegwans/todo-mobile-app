413a7af01748f45b0a5406c8399e0d951ad5932e
jaegwans <samidg108@gmail.com>
2021-12-31 19:04:14 +0900

    first commit

---
+++ src/components/Input.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
+import PropTypes from 'prop-types'
+
+const StyledInput = styled.TextInput.attrs(({theme})=>({
+    placeholderTextColor:theme.main,
+}))`
+width:${({width})=>width -40}px;
+height: 40px;
+margin: 3px 0;
+padding:5px 14px;
+border-radius:10px;
+background-color:${({theme})=>theme.itemBackground};
+font-size:20px;
+color:${({theme})=>theme.text};
+
+`;
+
+const Input= ({placeholder,value,onChangeText,onSubmitEditing,onBlur,})=>{
+
+    const width = Dimensions.get('window').width;
+
+    return (<StyledInput width={width} 
+    placeholder={placeholder}
+    autoCapitalize="none"
+    autoCorrect={false}
+    returnKeyType="done"
+    keyboardAppearance="dark"
+    value={value}
+    onChangeText={onChangeText}
+    onSubmitEditing={onSubmitEditing}
+    onBlur={onBlur}
+    
+     />);
+}
+Input.propTypes ={
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChangeText: PropTypes.func.isRequired,
+    onSubmitEditing: PropTypes.func.isRequired,
+    onBlur:PropTypes.func.isRequired,
+
+}
+
+export default Input; 
